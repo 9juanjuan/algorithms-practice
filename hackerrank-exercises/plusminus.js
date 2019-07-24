0a760985dd66dc66d394b410ad3878ef495925a3
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-07-24 19:08:27 -0400

    adds description to plusMinus problem

--- hackerrank-exercises/plusminus.js
+++ hackerrank-exercises/plusminus.js
@@ -1,4 +1,5 @@
- 
+// Given an array of integers, calculate the fractions of its elements that are positive, negative, and are zeros. Print the decimal value of each fraction on a new line.
+
    function plusMinus(arr) { 
     let pos = 0;
     let neg = 0;
