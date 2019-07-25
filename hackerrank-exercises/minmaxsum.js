dd781dc6ec134efe21637a9e340b7ab650f20c93
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-07-25 13:00:10 -0400

    changes to new solution that passes more test cases

--- hackerrank-exercises/minmaxsum.js
+++ hackerrank-exercises/minmaxsum.js
@@ -1,21 +1,23 @@
 // Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
 
 function miniMaxSum(arr) {
-        let max = Math.max(...arr);
-        let min = Math.min(...arr);
-        let maxSum = 0;
-        let minSum = 0;
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] !== min) {
-                maxSum += arr[i]
-            }
-            if (arr[i] !== max) {
-                minSum += arr[i]
-            }
-            if (arr[i] === min) {
-            }
+    let max = Math.max(...arr);
+    let min = Math.min(...arr);
+    let maxSum = 0;
+    let minSum = 0;
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+        if (arr[i] < min) {
+            min = arr[i]
         }
-        console.log (minSum+ ' '+maxSum)
+        else if (arr[i] > max) {
+            max = arr[i]
+        }
     }
+    maxSum = sum - min;
+    minSum = sum - max; 
+    console.log(minSum + ' ' + maxSum)
+}
     
     // Output:          10 14
