51c923ebc99e8ee20ddb0a272ea23538ab13fefe
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-08-25 11:15:35 -0400

    solves for maxProfit coding exercise

---
+++ practice/maxProfit.js
@@ -0,0 +1,31 @@
+// Given an array of stock prices, find the minimum buy price and the maximum sell price that produce the greatest profit.
+
+function maxProfit(prices) {
+    // determine what the base min buy price is 
+    let minBuyPrice = prices[0] < prices[1] ? prices[0] : prices[1];
+    // determine what the base max sell price is 
+    let maxSellPrice = prices[0] < prices[1] ? prices[1] : prices[2];
+    // sell price - buy price 
+    let maxProfit = maxSellPrice - minBuyPrice;
+    // temp holder for buy price
+    let tempBuyPrice = minBuyPrice;
+
+    for (let index = 2; index < prices.length; index++) {
+        const sellPrice = prices[index];
+
+        if (minBuyPrice > sellPrice) {
+            tempBuyPrice = prices[index];
+        } else {
+            const profit = sellPrice - minBuyPrice;
+            if (profit > maxProfit) {
+                maxProfit = profit;
+                maxSellPrice = sellPrice;
+                minBuyPrice = tempBuyPrice;
+            }
+        }
+    }
+
+    return [minBuyPrice, maxSellPrice];
+}
+
+console.log(maxProfit([1,2,3,4,5]))
